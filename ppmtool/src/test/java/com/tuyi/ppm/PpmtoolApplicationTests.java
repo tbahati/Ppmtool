0327e40c280be72bde60d575f87dac0d6d1d2c61
tbahati <tuyijean2003@gmail.com>
2020-08-20 09:47:37 +0200

    first commit

---
+++ ppmtool/src/test/java/com/tuyi/ppm/PpmtoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuyi.ppm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmtoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
